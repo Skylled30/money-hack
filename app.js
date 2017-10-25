c240b738088e26824e62ea05828011ac842758b5
Alex <vanteevs@mail.ru>
2017-10-25 20:21:33 +0800

    Added setdata

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const Web3 = require("web3");
 
+const contract = require("./contract");
+
 const app = express();
 const web3 = new Web3();
 
+const { eth } = web3;
+
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
-console.log(web3.eth.coinbase)
+const transaction = "0x78ee068ec5608f34f3cdc287c7e56bf2f764d0f756b318f5b6352c96873db23f"
+
+console.log(eth.coinbase);
+console.log(eth.getTransaction(transaction))
+
+const helloContract = web3.eth.contract(contract.ABI).at(contract.address);
 
 app.get("/", function(req, res) {
-	res.send("Hello");
+	const { personal } = web3;
+
+	web3.personal.unlockAccount(eth.coinbase, "123", 1000);
+
+	res.send(helloContract.setData.sendTransaction(
+		"Hell", 
+		{ from: eth.coinbase})
+	);
 });
 
 app.listen(8000, function() {
